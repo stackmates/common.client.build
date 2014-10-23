4ce3542dc511ae492317197e027f86dc5cb6b43c
Matt Mischewski <matt@dreamineering.com>
2014-10-23 10:35:35 +0200

    browserify with fs

--- tasks/browserify.js
+++ tasks/browserify.js
@@ -43,36 +43,6 @@ var wwwDir           = cfg.uri[cfg.siteBuild].ghpages;
 var entryPoint       = cfg.uri[cfg.siteBuild].browserifyEntry;
 
 
-// TODO what HTML from here or check out using BRFS to require
-// template where needed
-
-
-// var gulp = require('gulp');
-// var gutil = require('gulp-util');
-// var source = require('vinyl-source-stream');
-// var watchify = require('watchify');
-// var browserify = require('browserify');
-
-// gulp.task('watch', function() {
-//   var bundler = watchify(browserify('src/index.js', watchify.args));
-
-//   // Optionally, you can apply transforms
-//   // and other configuration options on the
-//   // bundler just as you would with browserify
-//   bundler.transform('brfs');
-
-//   bundler.on('update', rebundle);
-
-//   function rebundle() {
-//     return bundler.bundle()
-//       // log errors if they happen
-//       .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-//       .pipe(source('bundle.js'))
-//       .pipe(gulp.dest('./dist'));
-//   }
-
-//   return rebundle();
-// });
 
 function browserifyBundle () {
 
@@ -142,3 +112,25 @@ gulp.task('scripts', ['browserifySite'],  function () {
 
 gulp.task('browserifySite', browserifyBundle );
 gulp.task('browserifyAngular', ['templates'] , browserifyBundle );
+
+
+// gulp.task('watch', function() {
+//   var bundler = watchify(browserify('src/index.js', watchify.args));
+
+//   // Optionally, you can apply transforms
+//   // and other configuration options on the
+//   // bundler just as you would with browserify
+//   bundler.transform('brfs');
+
+//   bundler.on('update', rebundle);
+
+//   function rebundle() {
+//     return bundler.bundle()
+//       // log errors if they happen
+//       .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+//       .pipe(source('bundle.js'))
+//       .pipe(gulp.dest('./dist'));
+//   }
+
+//   return rebundle();
+// });
